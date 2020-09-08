284ccb9ae35efcd003a1a1e628f3b2b0a2bb0d7b
helloca <815612516@qq.com>
2020-09-08 11:00:04 +0800

    'update'

--- lib/sliver/test2.dart
+++ lib/sliver/test2.dart
@@ -4,15 +4,15 @@ class SliverAppBarTestPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("appbar"),
-      ),
+      backgroundColor: Colors.purpleAccent,
+//      appBar:
+//          AppBar(title: Text("appbar"), backgroundColor: Colors.transparent),
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
             floating: true,
             expandedHeight: 200,
-            title: Text("sliver app bar"),
+//            title: Text("sliver app bar"),
             leading: IconButton(
               icon: Icon(Icons.list),
             ),
@@ -27,7 +27,7 @@ class SliverAppBarTestPage extends StatelessWidget {
             flexibleSpace: FlexibleSpaceBar(
               title: Text("flexible space bar"),
               background: Image.network(
-                "https://dss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2534506313,1688529724&fm=26&gp=0.jpg",
+                "https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3892521478,1695688217&fm=26&gp=0.jpg",
                 fit: BoxFit.cover,
               ),
             ),
